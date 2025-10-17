40e11d5745ed7e27628e817f5de5d54f1ffb3290
Cherry ann Nepomuceno <57819565+cherryann28@users.noreply.github.com>
2025-10-17 16:02:36 -0700

    fix photo

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- <div class="bg-[#f3f5f6] text-[#1b3164] border-t-2 border-[#e0e1e2]"> -->
   <div class="bg-[#f3f5f6] text-[#124e86] border-t-2 border-[#e0e1e2] border-b-4">
     <div class="flex flex-col items-center justify-center gap-2 md:gap-6 max-w-xl mx-auto px-4 py-8 md:py-16" data-aos="fade-up">
       <h2 class="font-alex text-3xl md:text-4xl lg:text-5xl xl:text-6xl">Kindly</h2>
@@ -8,11 +9,14 @@
       <a href="https://docs.google.com/forms/d/14aC3TWy41O9VxfNS_DgXowdKWpJlTeTAm8gTeUgv0Xw/edit" target="_blank" rel="noopener noreferrer" class="font-jomolhari bg-[#22619A] hover:bg-[#1f2b8f] hover:shadow-lg hover:shadow-blue-300/40 transition duration-300 mt-5 text-white md:mt-2 text-base md:text-lg lg:text-xl px-16 sm:px-20 py-1 inline-block relative">
         RSVP
       </a>
+
+      <!-- <p class="text-[#0a0f3a] text-lg sm:text-xl md:text-2xl lg:text-2xl mt-8 font-bold font-jomolhari">BY THE NOVEMBER 3<sup>rd</sup> 2025</p> -->
       <p class="text-[#0f487e] text-base sm:text-lg md:text-xl lg:text-2xl mt-8 font-bold font-jomolhari">By the 15<sup>th</sup> of November, 2025</p>
       <p class="text-[15px] md:text-lg lg:text-xl mt-2 font-jomolhari italic text-center text-[#23557e]">Please click the RSVP button & kindly fill out the form to confirm your attendance</p>
     </div>
     <div class="mt-4">
       <div class="min-h-[300px] md:min-h-[500px] lg:min-h-[700px] xl:min-h-[1000px]  flex items-center bg-[url('/images/footer-bg.jpg')] bg-no-repeat bg-cover bg-[100%_50%]"></div>
+      <!-- <div class="absolute top-0 right-0 bottom-0 left-0 bg-[#15364D] opacity-80"></div> -->
     </div>
 
   </div>
--- components/Gallery.vue
+++ components/Gallery.vue
@@ -70,3 +70,4 @@ const options = {
     </div>
   </div>
 </template>
+<!-- yarn add @splidejs/splide-extension-grid @splidejs/vue-splide -->
